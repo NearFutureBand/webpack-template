6dba3e331915919507377da01ef927091ebb23ea
Pavel.Belyakov <Pavel.Beliakov@itechart-group.com>
2018-12-19 13:48:13 +0300

    react engine

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>React Starter Template</div>
+    );
+  }
+}
+export default App;
